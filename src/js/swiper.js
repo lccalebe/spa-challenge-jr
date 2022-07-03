3d26f2bb7a229b8f4f423e350d5b5b9066a0dd3a
Lucas <lukscalebe@hotmail.com>
2022-07-03 20:20:35 -0300

    projeto feito hj

---
+++ src/js/swiper.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
