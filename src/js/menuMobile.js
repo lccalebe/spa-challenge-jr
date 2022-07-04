38d0642c16b4d2662dfeb81c6e6290bb362b5999
Lucas <lukscalebe@hotmail.com>
2022-07-04 17:31:10 -0300

    add onmouseleave function

--- src/js/menuMobile.js
+++ src/js/menuMobile.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector('.menu');
 const btnMenu = document.querySelector('.btnMenu');
+const header = document.querySelector('.headerContainer');
 
 function abreMenu() {
     menu.style.display = 'flex';
     menu.style.flexDirection = 'column';
     btnMenu.setAttribute('onclick', 'fechaMenu()');
+    header.setAttribute('onmouseleave', 'fechaMenu()');
 };
 
 function fechaMenu() {
